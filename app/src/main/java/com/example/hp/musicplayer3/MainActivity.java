d9269ac3237b724f7b9a9ebedfc66986aa80278b
SS-AH-IB <triode.sai@gmail.com>
2017-07-06 02:25:18 +0530

    More Accurate

--- app/src/main/java/com/example/hp/musicplayer3/MainActivity.java
+++ app/src/main/java/com/example/hp/musicplayer3/MainActivity.java
@@ -1,29 +1,47 @@
 package com.example.hp.musicplayer3;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
-import android.content.Context;
+import android.content.ContentUris;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.jar.Manifest;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
     private static final int MY_PERMISSION_REQYEST=1;
-    ArrayList<String> arrayList;
+    ArrayList<String> arrayList=new ArrayList<String>();
+    ArrayList<File> allSongs=new ArrayList<File>();
+    ArrayList<Bitmap> allAlbums=new ArrayList<Bitmap>();
     ListView listView;
     ArrayAdapter<String> adapter;
+    static MediaPlayer mp;
+    ImageView albumarts;
+    TextView albumid;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,30 +56,99 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         else {
-            dostuff();
+            new loadingSongs().execute();
         }
     }
+
+    public class loadingSongs extends AsyncTask<File,Void,Void> {
+
+        private ProgressDialog dialog;
+
+        @Override
+        protected Void doInBackground(File... voids) {
+            getmusic();
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog=new ProgressDialog(MainActivity.this);
+            dialog.setTitle("Loading");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            dialog.dismiss();
+            dostuff();
+        }
+
+    }
+
+
+
+
     public void dostuff()
     {
         listView=(ListView)findViewById(R.id.listview);
-        arrayList=new ArrayList<>();
-        getmusic();
+        albumarts=(ImageView)findViewById(R.id.albumarts);
+        albumid=(TextView)findViewById(R.id.albumid);
         adapter=new ArrayAdapter<>(getApplicationContext(),R.layout.list,R.id.text,arrayList);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Uri u=Uri.parse(allSongs.get(i).toString());
+                mp= MediaPlayer.create(getApplicationContext(),u);
+                mp.start();
+                albumarts.setImageBitmap(allAlbums.get(i));
+            }
+        });
     }
+
+
     public void getmusic()
     {
         ContentResolver cr=getContentResolver();
         Uri songUri= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Uri albumUri=MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI;
+
+        final Uri ART_CONTENT_URI=Uri.parse("content://media/external/audio/albumart");
+
+        Uri artUri;
+
         Cursor songCursor = cr.query(songUri,null,null,null,null);
-        if(songCursor!=null && songCursor.moveToFirst())
+        Cursor albumCursor = cr.query(albumUri,null,null,null,null);
+
+        if(songCursor!=null && albumCursor!=null && songCursor.moveToFirst() && albumCursor.moveToFirst())
         {
             int songTitle=songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int songArtist=songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int songPosition=songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+
+
             do{
+                long songAlbumid=songCursor.getLong(songCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID));
+                artUri= ContentUris.withAppendedId(ART_CONTENT_URI,songAlbumid);
+                Bitmap album_art_bitmap=null;
+                try{
+
+                    album_art_bitmap=(MediaStore.Images.Media.getBitmap(getContentResolver(),artUri));
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 String currentTitle = songCursor.getString(songTitle);
                 String currentArtist= songCursor.getString(songArtist);
                 arrayList.add(currentTitle + "\n" + currentArtist);
+                File singlesong=new File(songCursor.getString(songPosition));
+
+                allSongs.add(singlesong);
+                allAlbums.add(album_art_bitmap);
+
+
             }while (songCursor.moveToNext());
         }
 
@@ -76,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                    if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)==
                            PackageManager.PERMISSION_GRANTED){
                        Toast.makeText(this,"permission granted",Toast.LENGTH_SHORT).show();
-                       dostuff();
+                       new loadingSongs().execute();
                    }
                }
                else
---
+++ app/src/main/java/com/example/hp/musicplayer3/loadingSongs.java
@@ -0,0 +1,33 @@
+package com.example.hp.musicplayer3;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by dell on 04-Jul-17.
+ */
+
+public class loadingSongs extends AsyncTask<Void,Void,Void> {
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+
+}
